257ab8f7a7f60264c4c964830c6e913cb0f9f822
Dmitriy <me@catwhocode.ga>
2015-09-03 00:50:27 +0600

    First steps with celestia provider

--- system/core/providers/celestia_provider.js
+++ system/core/providers/celestia_provider.js
@@ -28,12 +28,17 @@
     SYS
       IDENTIFY
       FLUSH
+      
+  AltStruct::CelestiaGET
+  
+    
 
 */
 
 // Require some modules
 var redisClient = require('../redisFactory.js');
 var indigoError = require('../particles/indigoError.js');
+var superagent  = require('superagent') ;
 
 // GPIO
 var gpio = {};
@@ -51,8 +56,70 @@ gpio.dwrite = function (deviceName, pinNumber, pinValue, callback) {
   if(deviceName.length < 3 || !(/^[\w.@]+$/).test(deviceName)) {
   
     // Return error callback
-    //return callback(new );
+    return callback(new indigoError({ message: 'Incorrect device name', errorCode: 400 }));
   
   }
+  
+  // Check port and value
+  if(typeof(pinNumber) != 'integer' || typeof(pinValue) != 'integer' || (pinValue === 0 || pinValue === 1) ) {
+     
+    // Return error callback
+    return callback(new indigoError({ message: 'Incorrect port or state', errorCode: 400 }));
+     
+  }
+  
+  // Get device data from DB
+  redisClient.get('rhcs:devices:' + deviceName, function (err, data) {
+  
+    // Catch error
+    if(err) {
+    
+      // Log it
+      log.error('Redis ' + err);
+      
+      // Return error callback
+      return callback(new indigoError({ message: err, errorCode: 500 }));
+    
+    }
+    
+    // Device not exists
+    if(!data) {
+    
+      // Return error callback
+      return callback(new indigoError({ message: 'Device not exists', errorCode: 404 }));
+    
+    }
+    
+    // Device exists
+    // Parse JSON from DB
+    data = JSON.parse(data);
+    
+    // Make request
+    superagent.get('http://' + data.ip + '/' + data.password + '::GPIO' + pinNumber + ':SET:' + pinValue).end(function (err, data) {
+    
+      // Catch timeout error
+      if(err.timeout) {
+      
+        // Return error callback
+        return callback(new indigoError({ message: 'Device unresponsible', errorCode: 503 }));
+      
+      }
+      
+      else if(err) {
+      
+        // Return error callback
+        return callback(new indigoError({ message: err, errorCode: 400 }));
+      
+      }
+      
+      else {
+      
+        /* parse data & return callback */
+      
+      }
+    
+    });
+  
+  });
 
 }
