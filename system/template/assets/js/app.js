f4612232c6a2f3963b4c1cc447c992f0d207869b
Dmitriy <me@catwhocode.ga>
2015-09-06 00:17:21 +0600

    Improvments in UI

--- system/template/assets/js/app.js
+++ system/template/assets/js/app.js
@@ -50,4 +50,84 @@ $('#loginForm').on('submit', function (event) {
   
 });
 
+// Sidebar menu
 $(".button-collapse").sideNav();
+
+// Update time widget
+if(window.rhcs.pageMarker == 'main') {
+
+ /**
+  * updateDateWidget() - update date widget on dashboard
+  * @param {String} [domElementName="dashboard-widget-date"] DOM Element ID (default: dashboard-widget-date)
+  */
+  
+  function updateDateWidget(domElementName) {
+
+    // Default argument value
+    domElementName = domElementName || "dashboard-widget-date";
+
+    // Getting current date
+    var date = new Date();
+
+    // Months names
+    var month = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+
+    // Returnable string
+    var parsedDate = '&nbsp;' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear();
+
+    // Append new value
+    document.getElementById(domElementName).innerHTML = parsedDate;
+
+    // Exit
+    return;
+
+  }
+
+  
+  
+  /**
+   * updateTimeWidget() - update time widget on dashboard
+   * @param {String} [domElementName="dashboard-widget-time"] DOM Element ID (default: dashboard-widget-date)
+   */
+  
+  function updateTimeWidget(domElementName) {
+
+    // Default argument value
+    domElementName = domElementName || "dashboard-widget-time";
+
+    // Getting date
+    var date = new Date();
+
+    // Final result value
+    var parsedDate = '';
+
+    // Each parameter (hour, min, sec) should have leading zero
+    if(date.getHours() < 10) { parsedDate += '0'; }
+    parsedDate += date.getHours() + ':';
+
+    if(date.getMinutes() < 10) { parsedDate += '0'; }
+    parsedDate += date.getMinutes() + ':';
+
+    if(date.getSeconds() < 10) { parsedDate += '0'; }
+    parsedDate += date.getSeconds();
+
+    // Append new DOM value
+    document.getElementById(domElementName).innerHTML = parsedDate;
+
+    // Exit
+    return;
+
+  }
+
+  // First run
+  updateTimeWidget();
+  updateDateWidget();
+  
+  // Set timer for do this
+  setInterval(updateTimeWidget, 1000);
+  setInterval(updateDateWidget, 60000);
+  
+  // In default mode we just show the screenshot
+  document.getElementById('cameraStream').setAttribute('src', 'http://' + window.location.hostname + ':8080/?action=stream');
+
+}
